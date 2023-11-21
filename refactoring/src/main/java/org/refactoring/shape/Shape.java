58d9da4b95961540e348ae8ad5f1d355291551ab
El Hadji Ahmadou GUEYE <128684816+codecraftingwarrior@users.noreply.github.com>
2023-11-21 10:46:38 +0100

    added comment with default

--- refactoring/src/main/java/org/refactoring/shape/Shape.java
+++ refactoring/src/main/java/org/refactoring/shape/Shape.java
@@ -2,6 +2,9 @@ package org.refactoring.shape;
 
 import java.awt.*;
 
+// Défaut : La logique métier de la methode LogDrawing s'intègre mieux dans la classe Logger, qui est ici transmis en tant que paramètre de méthode.
+// Opération de Refactoring Visée : "Move instance method"
+
 public class Shape
 {
     private Point pivot;
