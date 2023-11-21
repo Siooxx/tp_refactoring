032dd7b6ed5ad9dd217acb882cbe7fbfd01c3179
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-11-21 10:29:40 +0100

    continuing

--- refactoring/src/main/java/org/example/animals/Cat.java
+++ refactoring/src/main/java/org/example/animals/Cat.java
@@ -1,4 +1,8 @@
 package org.example.animals;
 
 public class Cat {
+
+    public void makeSound() { System.out.println("Miaou"); }
+
+    public void chaseMouse() { System.out.println("Le chat chasse une souris."); }
 }
--- refactoring/src/main/java/org/example/animals/Dog.java
+++ refactoring/src/main/java/org/example/animals/Dog.java
@@ -2,5 +2,7 @@ package org.example.animals;
 
 public class Dog {
 
-
+    public void makeSound() { System.out.println("Wouaf"); }
+    
+    public void fetchBall() { System.out.println("Le chien rapporte la balle."); }
 }
