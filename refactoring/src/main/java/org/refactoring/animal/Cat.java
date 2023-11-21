b1c89e48a367e25252271f85b86b6367bcbf9036
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-11-21 10:37:03 +0100

    adding getter setter

--- refactoring/src/main/java/org/refactoring/animal/Cat.java
+++ refactoring/src/main/java/org/refactoring/animal/Cat.java
@@ -2,7 +2,18 @@ package org.refactoring.animal;
 
 public class Cat {
 
+    private String name;
+    private String color;
+
     public void makeSound() { System.out.println("Miaou"); }
 
     public void chaseMouse() { System.out.println("Le chat chasse une souris."); }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getColor() { return color; }
+
+    public void setColor(String color) { this.color = color; }
 }
--- refactoring/src/main/java/org/refactoring/animal/Dog.java
+++ refactoring/src/main/java/org/refactoring/animal/Dog.java
@@ -2,7 +2,18 @@ package org.refactoring.animal;
 
 public class Dog {
 
+    private String name;
+    private String color;
+
     public void makeSound() { System.out.println("Wouaf"); }
 
     public void fetchBall() { System.out.println("Le chien rapporte la balle."); }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getColor() { return color; }
+
+    public void setColor(String color) { this.color = color; }
 }
