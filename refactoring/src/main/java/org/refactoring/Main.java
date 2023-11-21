2199f35924fb9b05d18d4b049f94962faa01bf5c
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-11-21 22:13:40 +0100

    cleaning code

--- refactoring/src/main/java/org/refactoring/Main.java
+++ refactoring/src/main/java/org/refactoring/Main.java
@@ -5,7 +5,6 @@ import org.refactoring.animal.Dog;
 import org.refactoring.shape.Logger;
 import org.refactoring.shape.Shape;
 
-
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -15,8 +14,7 @@ public class Main {
         cat.makeSound();
         dog.fetchBall();
         cat.chaseMouse();
-
-
+        
         Shape shape = new Shape();
         new Logger().LogDrawing(shape);
     }
