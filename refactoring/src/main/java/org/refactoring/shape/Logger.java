d885f17f079b52e008de9d0277757823f5aa2fe9
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-11-21 14:30:11 +0100

    adding comments

--- refactoring/src/main/java/org/refactoring/shape/Logger.java
+++ refactoring/src/main/java/org/refactoring/shape/Logger.java
@@ -1,5 +1,8 @@
 package org.refactoring.shape;
 
+// Défaut : La logique métier de la methode LogDrawing s'intègre mieux dans la classe Logger, qui est ici transmis en tant que paramètre de méthode.
+// Opération de Refactoring Visée : "Move instance method"
+
 public class Logger {
     public void Log(String msg) { System.out.println(msg); }
 }
