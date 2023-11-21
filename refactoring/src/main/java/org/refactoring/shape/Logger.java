ce74165573e3ec18cff2911d98e7838c0fc3a3fb
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-11-21 11:17:37 +0100

    cleaning code

--- refactoring/src/main/java/org/refactoring/shape/Logger.java
+++ refactoring/src/main/java/org/refactoring/shape/Logger.java
@@ -1,7 +1,6 @@
 package org.refactoring.shape;
 
-public class Logger
-{
+public class Logger {
     public void Log(String msg)
     {
         System.out.println(msg);
--- refactoring/src/main/java/org/refactoring/shape/Shape.java
+++ refactoring/src/main/java/org/refactoring/shape/Shape.java
@@ -5,15 +5,13 @@ import java.awt.*;
 // Défaut : La logique métier de la methode LogDrawing s'intègre mieux dans la classe Logger, qui est ici transmis en tant que paramètre de méthode.
 // Opération de Refactoring Visée : "Move instance method"
 
-public class Shape
-{
+public class Shape {
     private Point pivot;
 
     public Shape() {
         pivot = new Point(1,2);
     }
-    public void LogDrawing(Logger logger)
-    {
+    public void LogDrawing(Logger logger) {
         String msg = String.format("Shape is drawn at %d,  %d", pivot.x, pivot.y);
         logger.Log(msg);
     }
